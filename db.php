c3f268733f1183c01e6f8d70de754d5239c0ba18
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-02-05 00:45:04 +0900

    [modify]php study file

---
+++ db.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+    <title>singup_ok</title>
+    <style>
+      body {
+        font-family: Consolas, monospace;
+        font-family: 12px;
+      }
+    </style>
+  </head>
+  <body>
+    <?php
+      $id = $_POST[ 'id' ];
+      $pass = $_POST[ 'pass' ];
+      $age = $_POST[ 'age' ];
+      $mail = $_POST[ 'mail' ];
+      if ( is_null( $id ) ) {
+        echo '<h1>Fail!</h1>';
+      } else {
+        $jb_conn = mysqli_connect( 'localhost', 'username', 'password', 'signup' );
+        $jb_sql = "INSERT INTO usertbl ( id, pass, age, mail ) VALUES ( '$id', '$pass', '$age', '$mail', '$gender', '$hire_date' );";
+        mysqli_query( $jb_conn, $jb_sql );
+        echo '<h1>Success!</h1>';
+      }
+    ?>
+    <p>
+      <a href="signup.php">signup</a>
+     </p>
+  </body>
+</html>
---
+++ home/signup.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+    <title>Add Employee</title>
+    <style>
+      body {
+        font-family: Consolas, monospace;
+        font-family: 12px;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>회원가입</h1>
+    <form action="db.php" method="POST">
+  <p><input type="text"   name=id placeholder="아이디" required></p>
+  <p><input type="number" name=pass placeholder="비밀번호" required></p>
+  <p><input type="number" name=age placeholder="나이"  required></p>
+  <p><input type="text"   name=mail placeholder="메일주소" required></p>
+  <button>ADD</button>
+</form>
+  </body>
+</html>
