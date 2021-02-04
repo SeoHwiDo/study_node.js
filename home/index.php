f51a1bee25c28f41af69e16707bb20839639c499
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-02-04 15:55:38 +0900

    [modify]php study file

--- home/index.php
+++ home/index.php
@@ -7,9 +7,17 @@
 </head>
 <body>
     <?php
-        echo date('Y-m-d H:i:s');
-        $name="test";
-        echo "hello".$name
+        echo date('Y-m-d H:i:s')."<br>";
+        $name="test<br>";
+        echo "hello".$name."<br>"
     ?>
+    <h1>PHP test</h1>
+    <ol>
+        <li><h3><a href="index.php?id=test1">test1</a></h3></li>
+        <li><h3><a href="index.php?id=test2">test2</a></h3></li>
+        <li><h3><a href="index.php?id=test3">test3</a></h3></li>
+    </ol>
+    <?php echo $_GET['id']; ?>
+
 </body>
 </html>
