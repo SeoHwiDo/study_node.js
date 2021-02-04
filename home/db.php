fdaef9d3ab04023fb0ca806a0110f5640fe59624
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-02-05 01:04:43 +0900

    [modify]php study file

--- home/db.php
+++ home/db.php
@@ -17,21 +17,16 @@
       $pass = $_POST[ 'pass' ];
       $age = $_POST[ 'age' ];
       $mail = $_POST[ 'mail' ];
-      $test=1;
-      echo $id;
-      if(is_null($test)){
-        echo 'point2';
-      }else{
-        echo 'point3';
-      }
-      if ( is_null( $id ) ) {
+     
+      if(is_null($id)){
         echo '<h1>Fail!</h1>';
-      } else {
-        $jb_conn = mysqli_connect( 'localhost', 'username', 'password', 'signup' );
-        $jb_sql = "INSERT INTO usertbl ( id, pass, age, mail ) VALUES ( '$id', '$pass', '$age', '$mail', '$gender', '$hire_date' );";
-        mysqli_query( $jb_conn, $jb_sql );
-        echo '<h1>Success!</h1>';
       }
+      // }else{
+      //   $jb_conn = mysqli_connect( 'localhost', 'username', 'password', 'signup' );
+      //   $jb_sql = "INSERT INTO usertbl ( id, pass, age, mail ) VALUES ( '$id', '$pass', '$age', '$mail', '$gender', '$hire_date' );";
+      //   mysqli_query( $jb_conn, $jb_sql );
+      //   echo '<h1>Success!</h1>';
+      // }
     ?>
     <p>
       <a href="signup.php">signup</a>
