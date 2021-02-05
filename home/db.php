cb3609d93d24960655f55037810928070510f08b
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-02-05 20:00:33 +0900

    [modify]php study file

--- home/db.php
+++ home/db.php
@@ -17,13 +17,7 @@
       $pass = $_POST[ 'pass' ];
       $age = $_POST[ 'age' ];
       $mail = $_POST[ 'mail' ];
-
-      $host = '222.110.111.54';
-      $user = 'geoni24';
-      $pw = '';
-      $dbName = 'geoni24';
-      $port = '14103';
-     
+     require 'hostinfo.php';
       // mysqli_connect_errno($mysqli)
       // $mysqli
       if(is_null($id)){
@@ -31,10 +25,10 @@
       
       }else{
         $mysqli = new mysqli($host, $user, $pw, $dbName, $port);
-        if (!$mysqli){
+        if (mysqli_connect_errno($mysqli)){
           echo "DB 연결 실패:" . mysqli_connect_error(); 
       }else{
-        $sql = "INSERT INTO usertbl ( id, pass, age, mail ) VALUES ( '$id', '$pass', '$age', '$mail', '$gender', '$hire_date' );";
+        $sql = "INSERT INTO userList ( id, pass, age, mail ) VALUES ( '$id', '$pass', '$age', '$mail', '$gender', '$hire_date' );";
         mysqli_query( $conn, $sql );
         echo '<h1>Success!</h1>';
       }
--- home/signup.php
+++ home/signup.php
@@ -14,7 +14,7 @@
     <h1>회원가입</h1>
     <form action="db.php" method="POST">
   <p><input type="text"   name=id placeholder="아이디" required></p>
-  <p><input type="text" name=pass placeholder="비밀번호" required></p>
+  <p><input type="password" name=pass placeholder="비밀번호" required></p>
   <p><input type="text" name=age placeholder="나이"  required></p>
   <p><input type="text"   name=mail placeholder="메일주소" required></p>
   <button>ADD</button>
