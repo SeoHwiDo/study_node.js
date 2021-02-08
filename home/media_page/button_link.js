b538ebb65586e8d0f6513bd459e409b077fb9bfa
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-02-08 23:10:47 +0900

    [modify] optimization mobile

--- home/media_page/button_link.js
+++ home/media_page/button_link.js
@@ -1,5 +1,21 @@
 document.write("<script type='text/javascript' src='media_list.js'><"+"/script>");
 
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+
+    if (isMobile()) {
+    //   alert('mobile');
+      document.write('<link rel="stylesheet" type="text/css" href="moButton.css">');
+    }else{
+    //   alert('pc');
+      document.write('<link rel="stylesheet" type="text/css" href="pcButton.css">');
+    }
+ 
+
+
+
 function changeVid(i,videos,n,m){
     var video = document.getElementById('video');
 	video.setAttribute("src", videos[n][m]);
