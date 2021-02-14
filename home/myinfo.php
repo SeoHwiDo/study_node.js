27c7273546b278536357acff58943e5cc0dbb7b3
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-02-14 17:15:51 +0900

    [modify] modify main page

--- home/myinfo.php
+++ home/myinfo.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php session_start(); ?>
+    <?php session_start(); 
+    $query = "select * from userList";
+    $result = $mysqli->query($query);
+    $row=mysqli_fetch_assoc($result);?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
