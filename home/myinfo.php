1063b478e86cc49a3384d9bc8abd6a4bec9ea9e9
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-02-14 16:35:58 +0900

    [modify] modify main page

--- home/myinfo.php
+++ home/myinfo.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
-<html lang="ko">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
     require 'login.php';
     if(mysqli_num_rows($result)==1) {  
@@ -16,15 +19,14 @@
         document.getElementById("permit").innerHTML="<?=$row['permit']?>"; 
     } 
 </script> 
-    <title>내정보</title>
-</head>
-<body>
 <div id='info'>
     <div id='userID'></div>
     <div id='age'></div>
     <div id='phone'></div>
     <div id='permit'></div>
 </div>
-
+<?php 
+    }
+?>
 </body>
 </html>
