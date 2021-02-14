a2715adb07bfbabb0e53ffbd24846adbee580b96
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-02-14 13:12:08 +0900

    [add] add login function

--- home/login.php
+++ home/login.php
@@ -1,55 +1,44 @@
 <?php
   require 'hostinfo.php';
   session_start();
-  $connect = mysqli_connect($host, $user, $pw, $dbName) or die("fail");
     //입력 받은 id와 password
     $logID=$_POST['logID'];
-    $oriPass = $_GET[ 'logPass' ];
-    $logPass=password_hash ( $oriPass , PASSWORD_DEFAULT );
+    $logPass = $_GET[ 'logPass' ];
     
  
         //아이디가 있는지 검사
-        $query = "select * from userList where userID='$logID'";
-        $result = $connect->query($query);
- 
- 
-        //아이디가 있다면 비밀번호 검사
-        if(mysqli_num_rows($result)==1) {
- 
-                $row=mysqli_fetch_assoc($result);
- 
-                //비밀번호가 맞다면 세션 생성
-                if($row['pass']==$logPass){
-                        $_SESSION['userID']=$logID;
-                        if(isset($_SESSION['userID'])){
-                        ?>      <script>
-                                        alert("로그인 되었습니다.");
-                                        location.replace("index.html");
-                                </script>
-<?php
-                        }
-                        else{
-                                echo "session fail";
-                        }
-                }
- 
-                else {
-        ?>              <script>
-                                alert("아이디 혹은 비밀번호가 잘못되었습니다.");
-                                history.back();
-                        </script>
-        <?php
-                }
- 
+        $query = "select * from userList where userID='{$logID}'";
+        $result = mysqli_query($mysqli, $query);
+
+        $row = mysqli_fetch_array($result);
+        $hashedPassword = $row['pass'];
+        $row['userID'];
+
+        foreach($row as $key => $r){
+        echo "{$key} : {$r} <br>";
         }
  
-                else{
-?>              <script>
-                        alert("아이디 혹은 비밀번호가 잘못되었습니다.");
-                        history.back();
-                </script>
+        $passwordResult = password_verify($logPass, $hashedPassword);
+        if ($passwordResult === true) {
+        // 로그인 성공
+        // 세션에 id 저장
+        session_start();
+        $_SESSION['userID'] = $row['userID'];
+        print_r($_SESSION);
+        echo $_SESSION['userID'];
+    
+?>
+        <script>
+        alert("로그인에 성공하였습니다.")
+        location.href = "media_page\media_index.html";
+        </script>
+<?php
+        } else {
+        // 로그인 실패 
+?>
+        <script>
+          alert("로그인에 실패하였습니다");
+        </script>
 <?php
         }
- 
- 
 ?>
