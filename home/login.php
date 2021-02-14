850634bb7ad1bc24cf87dcf18e60630330b99135
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-02-14 13:43:40 +0900

    [add] add login function

--- home/login.php
+++ home/login.php
@@ -1,44 +1,56 @@
 <?php
-  require 'hostinfo.php';
   session_start();
-    //입력 받은 id와 password
-    $logID=$_POST['logID'];
-    $logPass = $_GET[ 'logPass' ];
-    
+  require 'hostinfo.php';
+  //입력 받은 id와 password
+  $logID=$_POST['logID'];
+  $logPass=$_POST['logPass'];
  
-        //아이디가 있는지 검사
-        $query = "select * from userList where userID='{$logID}'";
-        $result = mysqli_query($mysqli, $query);
+  //아이디가 있는지 검사
+  $query = "select * from member where userID='$logID'";
+  $result = $mysqli->query($query);
 
-        $row = mysqli_fetch_array($result);
-        $hashedPassword = $row['pass'];
-        $row['userID'];
-
-        foreach($row as $key => $r){
-        // echo "{$key} : {$r} <br>";
-        }
- 
-        $passwordResult = password_verify($logPass, $hashedPassword);
-        if ($passwordResult === true) {
-        // 로그인 성공
-        // 세션에 id 저장
-        session_start();
-        $_SESSION['userID'] = $row['userID'];
-        // print_r($_SESSION);
-        // echo $_SESSION['userID'];
-    
-?>
+  //아이디가 있다면 비밀번호 검사
+  if(mysqli_num_rows($result)==1) {
+    $row=mysqli_fetch_assoc($result);
+    $hashedPassword = $row['pass'];
+    $passwordResult = password_verify($logPass, $hashedPassword);
+    //비밀번호가 맞다면 세션 생성
+    if($passwordResult===true){
+      $_SESSION['userid']=$logID;
+      if(isset($_SESSION['userid'])){
+?>      
         <script>
-        alert("로그인에 성공하였습니다.")
-        location.href = "media_page\media_index.html";
+          alert("로그인에 성공하였습니다.")
+          location.href = "media_page\media_index.html";
         </script>
 <?php
-        } else {
-        // 로그인 실패 
+      }
+      else{
 ?>
         <script>
           alert("로그인에 실패하였습니다");
+          location.href = "index.html";
         </script>
 <?php
-        }
+      }
+    }
+    else {
+?>         
+      <script>
+        alert("아이디 혹은 비밀번호가 잘못되었습니다.");
+        history.back();
+      </script>
+<?php
+    }
+ 
+ }
+ 
+  else{
+?>       
+    <script>
+      alert("아이디 혹은 비밀번호가 잘못되었습니다.");
+      history.back();
+    </script>
+<?php
+  } 
 ?>
