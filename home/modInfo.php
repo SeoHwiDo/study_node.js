8d67b856ede638ea14bdf96be7c39b9fa5e8089b
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-02-20 16:05:47 +0900

    [add] add modify userinfo func

--- home/modInfo.php
+++ home/modInfo.php
@@ -3,7 +3,7 @@
 
 <head>
 <?php 
-require 'function.php';
+session_start();
 require 'hostinfo.php';
 ?>
   <meta charset="utf-8">
@@ -18,32 +18,25 @@ require 'hostinfo.php';
 
 <body>
 <?php
-
-    $userid = $_SESSION['userid'];
-    $re_age = $_POST[ 're_age' ];
-    $re_phone = $_POST[ 're_phone' ];
-    $re_mail = $_POST[ 're_mail' ];
-    $re_oriPass = $_POST[ 're_pass' ];
-    $re_pass=password_hash ( $re_oriPass , PASSWORD_DEFAULT );
-
+    $userid=$_SESSION['userid'];
 ?>
   <h1>정보 수정</h1>
   <form>
-    <p><input type="password" name=re_pass placeholder="비밀번호">
+    <p><input type="password" name=re_pass placeholder="비밀번호" >
     <button 
-        onclick="<?php $sql = "UPDATE userList set pass = '$re_pass' WHERE userID = $userid ";
+        onclick="<?php   $re_oriPass = $_POST[ 're_pass' ]; $re_pass=password_hash ( $re_oriPass , PASSWORD_DEFAULT ); $sql = "UPDATE userList set pass = '$re_pass' WHERE userID = $userid ";
        	mysqli_query( $mysqli, $sql ); ?>">수정</button></p>
-    <p><input type="text" name=re_age placeholder="나이">
+    <p><input type="text" name=re_age placeholder="나이" >
     <button 
-        onclick="<?php $sql = "UPDATE userList set age = '$re_age' WHERE userID = $userid ";
+        onclick="<?php $re_age = $_POST[ 're_age' ]; $sql = "UPDATE userList set age = '$re_age' WHERE userID = $userid ";
        	mysqli_query( $mysqli, $sql ); ?>">수정</button></p>
-    <p><input type="text" name=re_phone placeholder="전화번호">
+    <p><input type="text" name=re_phone placeholder="전화번호" >
     <button 
-        onclick="<?php $sql = "UPDATE userList set phone  = '$re_phone' WHERE userID = $userid ";
+        onclick="<?php  $re_phone = $_POST[ 're_phone' ]; $sql = "UPDATE userList set phone  = '$re_phone' WHERE userID = $userid ";
        	mysqli_query( $mysqli, $sql ); ?>">수정</button></p>
-    <p><input type="text" name=re_mail placeholder="메일주소">
+    <p><input type="text" name=re_mail placeholder="메일주소" >
     <button 
-        onclick="<?php $sql = "UPDATE userList set mail  = '$re_mail' WHERE userID = $userid ";
+        onclick="<?php  $re_mail = $_POST[ 're_mail' ]; $sql = "UPDATE userList set mail  = '$re_mail' WHERE userID = $userid ";
        	mysqli_query( $mysqli, $sql ); ?>">수정</button></p>
     
   </form>
