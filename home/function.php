1c842feeb2b8cb70feaf8bf42feb609c0e8b1f41
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-02-20 14:14:42 +0900

    [modify] modify page permit

--- home/function.php
+++ home/function.php
@@ -1,7 +1,7 @@
 <script>
 function permitLink(phpfile,lev){
 
-    if(<?= $_SESSION['permit'] ?>=lev){
+    if(<?= $_SESSION['permit'] ?>>=lev){
         location.href=phpfile;
     }else{
         alert("권한이 없습니다. 관리자에게 문의하세요");
